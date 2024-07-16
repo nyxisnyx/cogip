c4ffa79865063ec1c3b143a1071cdec7ff1462b1
Stacy Druart <stacy@StacyDruartMSI>
2024-07-16 09:28:47 +0200

    maj

--- index.php
+++ index.php
@@ -3,23 +3,3 @@
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/Core/Helper.php';
 require_once __DIR__.'/Routes/Routes.php';
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/stylefooter.css">
-    
-    <title>Document</title>
-</head>
-<body>
-    
-<header>
-    <p class="Copyright">Copyright © 2022 • COGIP Inc.</p>
-    <script src="assets/scripts/footer.jsx">
-    </script>
-</header>
-</body>
-</html>
