ac67c9104c5009fc5ce665ee201c3e5a8f7cb61f
Stacy Druart <stacy@StacyDruartMSI>
2024-07-18 11:46:12 +0200

    maj

--- public/index.php
+++ public/index.php
@@ -3,13 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/stylefooter.css"> 
+    <link rel="stylesheet" href="/assets/css/footer.css"> 
     <link rel="stylesheet" href="/assets/css/darkmode.css">
     <title>React Footer with PHP</title>
 </head>
 <body>
     <div id="root"></div>
 
+    <!-- Inclure React et ReactDOM -->
+    <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
+    <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
+    <!-- Babel pour le support JSX -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
+
+    <!-- Votre code JSX -->
     <script type="text/babel">
         const Footer = () => {
             const handleClick = (buttonName) => {
@@ -18,35 +25,32 @@
 
             return (
                 <footer>
-                    <p className="Copyright">Copyright © 2024 • COGIP Inc.</p>
                     <span className="cogip">COGIP</span>
                     <div className="footer2">
-                    
                         <div className="contact">
-                        <a href="https://www.google.com/maps/search/?api=1&query=345+Faulconer+Drive,+Suite+4,+Charlottesville,+CA,+12345" target="_blank">
-                            <img src="/assets/img/icons/Location.svg" alt="localisation" />345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345
-                        </a>
+                            <a href="https://www.google.com/maps/search/?api=1&query=345+Faulconer+Drive,+Suite+4,+Charlottesville,+CA,+12345" target="_blank">
+                                <img src="/assets/img/icons/Location.svg" alt="localisation" />345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345
+                            </a>
                         </div>
-                        
                         <div className="contact2">
-                        <a href="tel:+1234567890">
-                            <img class="img-contact" src="/assets/img/icons/Icon_contact.svg" alt="contact" />
-                            <span class="span-contact">(123) 456-7890</span>
-                        </a>
-                        <a href="fax:+1234567890">
-                            <img class="img-contact" src="/assets/img/icons/Fax.svg" alt="fax" />
-                            <span class="span-contact">(123) 456-7890</span>
-                        </a>
+                            <a href="tel:+1234567890">
+                                <img className="img-contact" src="/assets/img/icons/Icon_contact.svg" alt="contact" />
+                                <span className="span-contact">(123) 456-7890</span>
+                            </a>
+                            <a href="fax:+1234567890">
+                                <img className="img-contact" src="/assets/img/icons/Fax.svg" alt="fax" />
+                                <span className="span-contact">(123) 456-7890</span>
+                            </a>
                         </div>
                         <div className="reseau">
-                            <a href="https://www.facebook.com" target="_blank"><img class="img-reseau" src="/assets/img/icons/Facebook.svg" alt="Facebook" /></a>
-                            <a href="https://www.Twitter.com" target="_blank"><img class="img-reseau" src="/assets/img/icons/Twitter.svg" alt="Twitter" /></a>
-                            <a href="https://www.LinkedIn.com" target="_blank"><img class="img-reseau" src="/assets/img/icons/LinkedIn.svg" alt="LinkedIn" /></a>
-                            <a href="https://www.Youtube.com" target="_blank"><img class="img-reseau" src="/assets/img/icons/Youtube.svg" alt="YouTube" /></a>
-                            <a href="https://www.Instagram.com" target="_blank"><img class="img-reseau" src="/assets/img/icons/Instagram.svg" alt="Instagram" /></a>
-                            <a href="https://www.GooglePlus.com" target="_blank"><img class="img-reseau" src="/assets/img/icons/GooglePlus.svg" alt="Google Plus" /></a>
-                            <a href="https://www.Pinterest.com" target="_blank"><img class="img-reseau" src="/assets/img/icons/Pinterest.svg" alt="Pinterest" /></a>
-                            <img class="img-reseau" src="/assets/img/icons/RSS.svg" alt="RSS" />
+                            <a href="https://www.facebook.com" target="_blank"><img className="img-reseau" src="/assets/img/icons/Facebook.svg" alt="Facebook" /></a>
+                            <a href="https://www.Twitter.com" target="_blank"><img className="img-reseau" src="/assets/img/icons/Twitter.svg" alt="Twitter" /></a>
+                            <a href="https://www.LinkedIn.com" target="_blank"><img className="img-reseau" src="/assets/img/icons/LinkedIn.svg" alt="LinkedIn" /></a>
+                            <a href="https://www.Youtube.com" target="_blank"><img className="img-reseau" src="/assets/img/icons/Youtube.svg" alt="YouTube" /></a>
+                            <a href="https://www.Instagram.com" target="_blank"><img className="img-reseau" src="/assets/img/icons/Instagram.svg" alt="Instagram" /></a>
+                            <a href="https://www.GooglePlus.com" target="_blank"><img className="img-reseau" src="/assets/img/icons/GooglePlus.svg" alt="Google Plus" /></a>
+                            <a href="https://www.Pinterest.com" target="_blank"><img className="img-reseau" src="/assets/img/icons/Pinterest.svg" alt="Pinterest" /></a>
+                            <img className="img-reseau" src="/assets/img/icons/RSS.svg" alt="RSS" />
                         </div>
                         <div className="btn-footer">
                             <a onClick={() => handleClick('Home')}>Home</a>
@@ -55,7 +59,7 @@
                             <a onClick={() => handleClick('Contact')}>Contact</a>
                             <a onClick={() => handleClick('Privacy Policy')}>Privacy Policy</a>
                         </div>
-                        
+                        <p className="Copyright">Copyright © 2024 • COGIP Inc.</p>
                     </div>
                 </footer>
             );
@@ -63,53 +67,5 @@
 
         ReactDOM.render(<Footer />, document.getElementById('root'));
     </script>
-
-    <!-- Inclure React et ReactDOM -->
-    <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
-    <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
-    <!-- Babel pour le support JSX -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
 </body>
 </html>
-
-
- 
- <!-- <?php
-
-// require_once __DIR__ . '/../index.php';
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
-?>--> 
-<!--<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/stylefooter.css"> 
-    <link rel="stylesheet" href="/assets/css/darkmode.css">
-    <script src="/public/assets/scripts/main.js" defer></script>
-    <title>Document</title>
-</head>
-
-<body>
-
-<div class="toggle-dark-mode" id="darkModeToggle">
-    
-        <img src="assets/img/icons/sun-moon.svg" alt="Light Mode" id="modeIcon">
-    </div>
-<script src="/assets/scripts/darkmode.js"></script>
-<script src="/assets/scripts/footer.jsx"></script>
-    <div id="root"></div>
-    
-</body>
-<footer>
-    <script>
-        
-    </script>
-
-</footer>
-</html>
-
- -->
-
