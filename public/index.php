d450cfe10008d048e41ef0aabbaa81fcadb5020e
Stacy Druart <stacy@StacyDruartMSI>
2024-07-18 15:06:01 +0200

    maj

--- public/index.php
+++ public/index.php
@@ -7,16 +7,17 @@
     <link rel="stylesheet" href="/assets/css/darkmode.css">
     <title>React Footer with PHP</title>
 </head>
-<body>
     <div id="root"></div>
-
+<body>
+    <script src="./assets/scripts/body.js"></script>
+</body>
     <!-- Inclure React et ReactDOM -->
     <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
     <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
     <!-- Babel pour le support JSX -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js"></script>
 
-    <!-- Votre code JSX -->
+    <!-- Votre code JSX pour le footer -->
     <script type="text/babel">
         const Footer = () => {
             const handleClick = (buttonName) => {
