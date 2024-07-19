da48d0ab40b0d5b19f60dce42ee67f55f4e5110a
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-07-19 12:05:08 +0200

    ajout de la methode register dans la class User et ajout de la methode createUser dans admin controller

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -5,8 +5,9 @@ namespace App\Controllers;
 use App\config\Database;
 use App\Controllers\CompaniesController;
 use App\Controllers\ContactsController;
+use App\Models\User;
 
-class AdminController{
+class AdminController {
 
     private Database $database;
 
@@ -19,12 +20,13 @@ class AdminController{
         try {
             $datasCompanies = (new CompaniesController($this->database))->getCompaniesDashbord($limit);
             $dataContacts =(new ContactsController($this->database))->getContactsDashbord($limit);
-            
+            $invoicesDatas =(new FacturesController($this->database))->getInvoicesDashbord($limit);
             $response = [
                 'status' => 202,
                 'message' => 'OK',
                 'Companies' => $datasCompanies,
-                'Contacts' => $dataContacts
+                'Contacts'=>$dataContacts,
+                'Invoices'=> $invoicesDatas
             ];
 
             echo createJson($response);
@@ -38,5 +40,16 @@ class AdminController{
             ];
             echo createJson($response);
         }
+    }
+
+    public function createUser(){
+
+        $json_str = file_get_contents('php://input');
+            // Get as an object
+        $json_obj = json_decode($json_str);
+            
+        $newUser = new User($json_obj->username,$json_obj->password);
+        $newUser->register($this->database);
+
     }
 }
--- Models/User.php
+++ Models/User.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 class User
 {
-
     private string $username;
+    private string $firstname;
+    private string $lastname;
     private string $email;
     private string $password;
     private int  $role;
 
-    public function __construct(string $username,  string $password, ?string $email, ?int $role)
+    public function __construct(string $username,  string $password, ?string $email =null, ?int $role=null, ?string $firstname=null, ?string $lastname=null)
     {
 
         $this->username = $username;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
         $this->email = $email;
         $this->password = $password;
         $this->role = $role;
@@ -25,6 +28,8 @@ class User
         $datainJson = json_encode(
             [
                 'username' => $this->username,
+                'firstname'=> $this->firstname,
+                'lastname'=>$this->lastname,
                 'email' => $this->email,
                 'password' => $this->password,
                 'role' => $this->role
@@ -35,8 +40,34 @@ class User
     }
 
 
-    public function register()
-    {
+    public function register($database)
+    {   
+        
+        try{
+            $newUserDatas=$database->query("INSERT INTO `users`( `user`, `first_name`, `last_name`, `email`, `password_hash`, `role_id`, `created_at`, `updated_at`)
+            VALUES (
+                    '{$this->username}',
+                    '{$this->firstname}',
+                    '{$this->lastname}',
+                    '{$this->email}',
+                    '{$this->password}',
+                    '{$this->role}',
+                    NOW(),
+                    NOW()
+                    )");
+    
+            echo createJson($newUserDatas);
+
+        } catch (\Throwable $th) {
+
+            $response = [
+                'status' => 400,
+                'message' => 'Bad Request',
+            ];
+            echo createJson($response);
+            echo $th;
+        }
+
     }
 
     public static function dataBodyInsert()
