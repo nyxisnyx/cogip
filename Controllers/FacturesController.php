cecb55c88bd6892470310736ff88ae811dac7873
Iliess <m.iliess.abdelmadjid@gmail.com>
2024-07-22 15:48:58 +0200

    update readme + companie != campanies

--- Controllers/FacturesController.php
+++ Controllers/FacturesController.php
@@ -183,7 +183,7 @@ class FacturesController extends Controller
             ];
 
             $invoicesDatas = $this->database->queryBindParam(
-                'SELECT invoices.*, campanies.name
+                'SELECT invoices.*, companies.name
                 FROM invoices
                 JOIN companies 
                 ON invoices.company_id = companies.company_id
