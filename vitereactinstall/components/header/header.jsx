a309ffc420d07739ccd7091990bfcb41b25195ae
JulieWustefeld <juliewustefeld@gmail.com>
2024-07-23 14:46:22 +0200

    fix header & login

--- vitereactinstall/components/header/header.jsx
+++ vitereactinstall/components/header/header.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
+import classNames from "classnames"
 import style from "./style.module.css"
+import { LoginPopover } from "./loginpopover/loginpopover"
 
 export const Header = () => {
     const [links, setLinks] = useState([{name:'Home', active:true}, {name:'Invoices', active:false}, {name:'Companies', active:false}, {name:'Contacts', active:false}]);
@@ -8,15 +10,18 @@ export const Header = () => {
 
     useEffect(() => {
         const copyLinks = links.map((link) => {
+            if (link.name === 'Home' && (window.location.pathname === "/" || window.location.pathname === "")) {
+                return {name:link.name, active:true};
+            }
             if (window.location.pathname.includes(link.name.toLowerCase())) {
                 return {name:link.name, active:true};
             }
             return {name:link.name, active:false};
         });
         
-        setLinks(copyLinks)
+        setLinks(copyLinks);
 
-        if (window.location.pathname === "/" || window.location.pathname === "/home") {
+        if (window.location.pathname === "/" || window.location.pathname === "/home" || window.location.pathname === "") {
             setIsHomePage(true);
         } else {
             setIsHomePage(false);
@@ -34,33 +39,21 @@ export const Header = () => {
     return (
         <header className={style.publicHeader}>
             {/* navbar here */}
-            <nav>
+            <nav className={classNames(!isHomePage?style.extraPadding:null)}>
                 <h1>Cogip</h1>
                 <menu>
                     {
                         links.map((link) => {
-                            return (<li className={link.active ? style.active : null}> <a href={`${link.name.toLowerCase()}`}>{link.name}</a></li>)
+                            return (<li key={link.name} className={link.active ? style.active : null}> <a href={`${link.name.toLowerCase()}`}>{link.name}</a></li>)
                         })
                     }
                 </menu>
 
                 <div>
-                {/* <button>Sign Up</button>  */}
-                {/* redirection */}
-                <button popovertarget="loginPopover">Log In</button>
-                <div popover="auto" id="loginPopover" className={style.loginPopover}>
-                
-                    <h3>Log In</h3>
-                    <form>
-                        <label for="email">Email address:</label>
-                        <input type="text" id="email" autocomplete="on" name="email"></input>
-
-                        <label for="password">Password:</label>
-                        <input id="password" type="password" autocomplete="on" name="password"></input>
-
-                        <button class="submitLog" type="submit">Login</button>
-                    </form>
-                </div>
+                    {/* <button>Sign Up</button>  */}
+                    {/* redirection */}
+                    
+                    <LoginPopover />
                 {/* popover */}
                 </div>
             </nav>
---
+++ vitereactinstall/components/header/loginpopover/loginpopover.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import style from "./style.module.css"
+
+export const LoginPopover = () => {
+    const [displayError, setDisplayError] = useState(false);
+    // Create handle submit function
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setDisplayError(false);
+        console.log('checking')
+        // console.log('test', e, e.target[0].value, e.target[1].value);
+        const body = {
+            "user" : e.target[0].value,
+            "password" : e.target[1].value
+        };
+        try {
+            const response = await fetch('http://becodecogip.rbarkersw.com/login', {method:'POST', body:JSON.stringify(body)});
+            const actualResponse = await response.json();
+            if (actualResponse.status === 401) {
+                setDisplayError(true)
+            } else if (actualResponse.status === 201) {
+                console.log("Authorized");
+                // Navigation goes here => window.location.href = 'url goes here'
+            };
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
+    return (
+        <>
+            <button popovertarget="loginPopover">Log In</button>
+            <div popover="auto" id="loginPopover" className={style.loginPopover}>
+                    
+                <h3>Log In</h3>
+                {displayError && <p className={style.messageUnauthorized}>Login Unsuccessful. You're not an authorized user.</p>}
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" autoComplete="on" name="username"></input>
+
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" type="password" autoComplete="on" name="password"></input>
+
+                    <button className={style.submitLog} type="submit">Login</button>
+                </form>
+            </div>
+        </>
+    )
+}
