2ad4c3654ee54e0f4fa05952e2555d71476cc58d
Stacy Druart <stacy@StacyDruartMSI>
2024-07-23 14:37:30 +0200

    maj

--- vitereactinstall/src/App.jsx
+++ vitereactinstall/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import '../../public/assets/css/styles.css'
-import Footer from './Footer';
+import Footer from '/Footer';
+import Client from'/Clients';
 
 
 const App = () => {
     return (
         <div className="App">
             <Footer />
+            <Client />
         </div>
     );
 };
