6005938f9ca4736329bffcfdd1d36427b45ba5ab
Stacy Druart <stacy@StacyDruartMSI>
2024-07-25 16:21:03 +0200

    maj

--- vitereactinstall/src/App.jsx
+++ vitereactinstall/src/App.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../public/assets/css/styles.css';
-import Footer from './Footer';
-import Client from'./Client';
+// import '../clients/Contact-List.css';
+import Footer from '../src/Footer';
+// import Clients from'../src/Client';
+// import ContactDetail from '../src/clients/ContactDetail';
 
+// import ContactDetail from '../clients/ContactDetail';
+import AllCompagnies from './clients/AllCompagnies';
+
+// import ShowContact from '../clients/ShowContact';
 
 const App = () => {
-    return (
-        <div className="App">
-            <Footer />
-            <Client />
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* // <Clients /> */}
+      {/* <ContactDetail /> */}
+      <AllCompagnies />
+      {/* <ShowContact />  */}
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
