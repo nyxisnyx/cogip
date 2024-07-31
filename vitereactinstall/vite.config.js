8c19de95bef654f9fb8be36fa142b18888b721a3
Tasha <nataliiamelnykova9@gmail.com>
2024-07-31 09:44:04 +0200

    db

--- vitereactinstall/vite.config.js
+++ vitereactinstall/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://becodecogip2.sysadminhell.com/',
+        target: 'http://becodecogip.yoyodyne.be/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
